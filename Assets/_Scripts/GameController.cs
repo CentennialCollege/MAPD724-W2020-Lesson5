2b933ddbce1290a88f6df70854243b8d11aecdf8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-02-05 22:32:43 -0500

    initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  public Transform cloudContainer;
+  public int cloudNum;
+  public GameObject cloud;
+
+  public List<GameObject> clouds;
+    // Start is called before the first frame update
+    void Start()
+    {
+      clouds = new List<GameObject>(); // empty list of type GameObject
+      _buildCloudList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void _buildCloudList()
+    {
+      for (int count = 0; count < cloudNum; count++)
+      {
+          var newCloud = Instantiate(cloud);
+          newCloud.transform.parent = cloudContainer.transform;
+          clouds.Add(newCloud);
+      }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _move();
+    }
+
+    private void _move()
+    {
+      Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+      transform.position = new Vector2 (Mathf.Clamp(mousePosition.x, -1.86f, 1.86f), -4.3f);
+    }
+}
